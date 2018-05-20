c5c05ca2a26eaf8c16c016ac3dbc5943b444c5bf
felixpy <felixpy.1993@gmail.com>
2018-05-20 17:18:44 +0800

    Add responsive navbar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function (global, $) {
   $(function () {
+    /* algolia search */
     var algoliaClient = algoliasearch('AQN226ZOWS', 'e57b0595befbd779c566b2a6defaf5c2')
     var algoliaIndexName = algoliaClient.initIndex('jekyll_blog');
 
@@ -22,6 +23,17 @@
       }
     }]).on('autocomplete:selected', function (event, suggestion, dataset) {
       window.location.href = suggestion.url;
+    });
+
+    /* page events */
+    var $navbar = $('#header-navbar');
+    var $navbarMenu = $navbar.find('.navbar-menu');
+
+    $navbar.on('click', '.navbar-burger', function() {
+      var $navbarBurger = $(this);
+      
+      $navbarMenu.toggleClass('is-active');
+      $navbarBurger.toggleClass('is-active');
     });
   });
 }(window, jQuery));
