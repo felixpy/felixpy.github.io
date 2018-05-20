8a08d9c8321ef49b30e9e5cd85b841995adee04f
felixpy <felixpy.1993@gmail.com>
2018-05-20 13:46:07 +0800

    Add algolia search

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function (global, $) {
+  $(function () {
+    var algoliaClient = algoliasearch('AQN226ZOWS', 'e57b0595befbd779c566b2a6defaf5c2')
+    var algoliaIndexName = algoliaClient.initIndex('jekyll_blog');
+
+    $('#header-navbar-search').autocomplete({
+      hint: false,
+      templates: {
+        dropdownMenu: '#algolia-dropdown-menu-template',
+        empty: '<div>Nothing here...</div>',
+        footer: '<div class="branding">Powered by <img src="https://www.algolia.com/static_assets/images/press/downloads/algolia-logo-light.svg" /></div>'
+      }
+    }, [{
+      source: $.fn.autocomplete.sources.hits(algoliaIndexName, {
+        hitsPerPage: 5
+      }),
+      displayKey: 'title',
+      templates: {
+        suggestion: function (suggestion) {
+          return suggestion._highlightResult.title.value;
+        }
+      }
+    }]).on('autocomplete:selected', function (event, suggestion, dataset) {
+      window.location.href = suggestion.url;
+    });
+  });
+}(window, jQuery));
